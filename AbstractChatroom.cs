f315ba6c553b66ed1a40495f29365595cfc407fb
Edward Vaklinov <edwardchorius@gmail.com>
2021-11-13 20:57:11 +0200

    'initialize'

---
+++ AbstractChatroom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediatorPattern
+{
+    abstract class AbstractChatroom
+
+    {
+        public abstract void Join(Participant participant);
+        public abstract void Send(
+          string from, string to, string message);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MediatorPattern
+{
+    class Program
+    {
+        static void Main()
+        {
+            var chatroom = new Chatroom();
+
+            Participant chatMember = new ConcreteParticipant("Georgy");
+
+            chatroom.Join(chatMember);
+
+            chatMember.Send("someone", "hey");
+        }
+    }
+}
